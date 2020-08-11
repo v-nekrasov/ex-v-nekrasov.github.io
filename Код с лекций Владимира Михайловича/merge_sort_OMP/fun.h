44855e380c5df633f56356d35cd05b33da16e9e8
Nekrasov-VV <vsevolod.nekrasov@math.msu.ru>
2020-08-12 01:16:21 +0500

    Delete fun.h

--- Код с лекций Владимира Михайловича/merge_sort_OMP/fun.h
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <time.h>
-void bubble_sort(int* m, int n);
-void merge(int *a, int n, int *b,int m, int *r);
-void merge_sort(int *a, int n, int *tmp);
-void merge_sort_OMP(int *a, int n, int *tmp);
