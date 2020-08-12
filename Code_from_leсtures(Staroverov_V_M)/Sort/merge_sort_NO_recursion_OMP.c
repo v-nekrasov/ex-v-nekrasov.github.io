660b41c6446fb5cc0b165f12b3b61a0e5a5b4d86
Nekrasov-VV <vsevolod.nekrasov@math.msu.ru>
2020-08-12 21:58:12 +0500

    Create merge_sort_NO_recursion_OMP.c

---
+++ Code_from_leсtures(Staroverov_V_M)/Sort/merge_sort_NO_recursion_OMP.c
@@ -0,0 +1,17 @@
+#include "header.h"
+void merge_sort_2_OMP(int *a, int n, int *tmp)//сортировка слиянием без рекурсии+OMP
+{
+	int k, k2, i;
+	for(k=1;k<n;k*=2)
+	{
+	#pragma omp parallel for private(k2)
+		for(i=0;i<n-k; i+=2*k)
+		{
+			k2=n-i-k;
+			if(k2>k)
+				k2=k;
+			merge(a+i, k, a+i+k, k2, tmp+i);
+			memcpy(a+i, tmp+i, (k+k2)*sizeof(a[0]));
+		}
+	}
+}
